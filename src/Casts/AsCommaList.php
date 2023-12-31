ac78128728e2498a2bf91657333cb89398f30790
Luan Himmlisch <luan@himmlisch.com.mx>
2023-12-31 14:59:50 -0600

    Abstracted CommaList class

--- src/Casts/AsCommaList.php
+++ src/Casts/AsCommaList.php
@@ -4,10 +4,7 @@ namespace WeblaborMx\TallUtils\Casts;
 
 use Illuminate\Contracts\Database\Eloquent\Castable;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Str;
-use InvalidArgumentException;
-use Traversable;
+use WeblaborMx\TallUtils\Classes\CommaList;
 
 class AsCommaList implements Castable
 {
@@ -23,51 +20,12 @@ class AsCommaList implements Castable
         {
             public function get($model, $key, $value, $attributes)
             {
-                if (is_string($value)) {
-                    $value = explode(',', $value);
-                }
-
-                if ($value instanceof Traversable) {
-                    $value = [...$value];
-                }
-
-                if (is_array($value)) {
-                    return $this->parseArray($value);
-                }
-
-                return collect();
+                return new CommaList($value);
             }
 
             public function set($model, $key, $value, $attributes)
             {
-                if (
-                    is_null($value) ||
-                    is_string($value)
-                ) {
-                    return $value;
-                }
-
-                if (is_scalar($value)) {
-                    return strval($value);
-                }
-
-                if ($value instanceof Traversable) {
-                    $value = [...$value];
-                }
-
-                if (is_array($value)) {
-                    return implode(',', $value);
-                }
-
-                throw new InvalidArgumentException('Value type can\'t be casted to CommaList', 1);
-            }
-
-            private function parseArray(array $array): Collection
-            {
-                return collect($array)
-                    ->filter(fn ($v) => is_scalar($v))
-                    ->map(fn ($v) => trim(strval($v)))
-                    ->filter(fn ($v) => $v !== '');
+                return (new CommaList($value))->toString();
             }
         };
     }
---
+++ src/Classes/CommaList.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace WeblaborMx\TallUtils\Classes;
+
+use ArrayAccess;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Contracts\Support\Jsonable;
+use Illuminate\Support\Collection;
+use JsonSerializable;
+use Livewire\Wireable;
+use Stringable;
+use Traversable;
+
+class CommaList implements
+    Stringable,
+    JsonSerializable,
+    Jsonable,
+    ArrayAccess,
+    Wireable
+{
+    /**
+     * The underlying array.
+     */
+    protected Collection $value;
+
+    public function __construct(Traversable|string|null $value)
+    {
+        $this->setValue($value);
+    }
+
+    public function setValue(Traversable|string|null $value): void
+    {
+        if (is_null($value)) {
+            $this->value = collect();
+        }
+
+        if (is_string($value)) {
+            $value = explode(',', $value);
+        }
+
+        if ($value instanceof Traversable) {
+            $value = [...$value];
+        }
+
+        $this->value = $this->parseArray($value);
+    }
+
+    /*
+     * Livewire
+     */
+
+    public static function fromLivewire($value): static
+    {
+        return new static($value);
+    }
+
+    public function toLivewire(): string
+    {
+        return $this->toString();
+    }
+
+    /*
+     * Castings
+     */
+
+    public function toString(): string
+    {
+        return (string) $this;
+    }
+
+    public function toJson($options = 0)
+    {
+        return json_encode($this, $options);
+    }
+
+    public function jsonSerialize(): string
+    {
+        return $this->__toString();
+    }
+
+    public function __toString(): string
+    {
+        return $this->value->implode(',');
+    }
+
+    /*
+     * Array Access
+     */
+
+    public function offsetExists(mixed $offset): bool
+    {
+        return $this->value->has($offset);
+    }
+
+    public function offsetGet(mixed $offset): mixed
+    {
+        return $this->value->get($offset);
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        $this->value[$offset] = $value;
+    }
+
+    public function offsetUnset(mixed $offset): void
+    {
+        unset($this->value[$offset]);
+    }
+
+
+    /*
+     * Internal
+     */
+
+    protected function parseArray(array $array): Collection
+    {
+        return collect($array)
+            ->filter(fn ($v) => is_scalar($v))
+            ->map(fn ($v) => trim(strval($v)))
+            ->filter(fn ($v) => $v !== '')
+            ->values();
+    }
+}
